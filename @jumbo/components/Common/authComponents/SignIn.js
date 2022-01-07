2ec47fee5c8ce0b84c9c4a72f0f1596e6785e4d8
= <=>
2022-01-07 10:19:51 -0400

    fixed null error providers

--- @jumbo/components/Common/authComponents/SignIn.js
+++ @jumbo/components/Common/authComponents/SignIn.js
@@ -1,43 +1,41 @@
-import React, { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
-import IntlMessages from '../../../utils/IntlMessages';
-import Button from '@material-ui/core/Button';
-import { Box } from '@material-ui/core';
-import { alpha, makeStyles } from '@material-ui/core/styles';
-import CmtImage from '../../../../@coremat/CmtImage';
-import Typography from '@material-ui/core/Typography';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from 'next/link';
-import AuthWrapper from './AuthWrapper';
-import { useAuth } from '../../../../authentication';
-import { NotificationLoader } from '../../ContentLoader';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import IntlMessages from "../../../utils/IntlMessages";
+import Button from "@material-ui/core/Button";
+import { Box } from "@material-ui/core";
+import { alpha, makeStyles } from "@material-ui/core/styles";
+import CmtImage from "../../../../@coremat/CmtImage";
+import Typography from "@material-ui/core/Typography";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "next/link";
+import AuthWrapper from "./AuthWrapper";
+import { useAuth } from "../../../../authentication";
+import { NotificationLoader } from "../../ContentLoader";
 
-import {  signIn } from "next-auth/react";
-
-
+import { signIn } from "next-auth/react";
 
 const useStyles = makeStyles((theme) => ({
   authThumb: {
     backgroundColor: alpha(theme.palette.primary.main, 0.12),
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '50%',
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "50%",
       order: 2,
     },
   },
   authContent: {
     padding: 30,
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: (props) => (props.variant === 'default' ? '50%' : '100%'),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: (props) => (props.variant === "default" ? "50%" : "100%"),
       order: 1,
     },
-    [theme.breakpoints.up('xl')]: {
+    [theme.breakpoints.up("xl")]: {
       padding: 50,
     },
   },
@@ -46,26 +44,29 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.primary,
   },
   textFieldRoot: {
-    '& .MuiOutlinedInput-notchedOutline': {
+    "& .MuiOutlinedInput-notchedOutline": {
       borderColor: alpha(theme.palette.common.dark, 0.12),
     },
   },
   formcontrolLabelRoot: {
-    '& .MuiFormControlLabel-label': {
-      [theme.breakpoints.down('xs')]: {
+    "& .MuiFormControlLabel-label": {
+      [theme.breakpoints.down("xs")]: {
         fontSize: 12,
       },
     },
   },
-  
 }));
 //variant = 'default', 'standard'
 // eslint-disable-next-line react/prop-types
-const SignIn = ({ variant = 'default', wrapperVariant = 'default' , providers}) => {
+const SignIn = ({
+  variant = "default",
+  wrapperVariant = "default",
+  providers,
+}) => {
   const classes = useStyles({ variant });
   const { isLoading, error, userLogin, userFacebookLogin } = useAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onSubmit = () => {
     userLogin({ username: email, password });
@@ -74,7 +75,6 @@ const SignIn = ({ variant = 'default', wrapperVariant = 'default' , providers}) 
   const onFacebookSubmit = () => {
     userFacebookLogin();
   };
-
 
   return (
     <AuthWrapper variant={wrapperVariant}>
@@ -154,7 +154,7 @@ const SignIn = ({ variant = 'default', wrapperVariant = 'default' , providers}) 
           </Box>
         </form>
         <Box mb={2} mx="auto" display="flex" justifyContent="center">
-          {Object.values(providers).map((provider) => (
+          {Object.values(providers || {}).map((provider) => (
             <div key={provider.name}>
               <button
                 className="loginBtn loginBtn--facebook"
