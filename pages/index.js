4d7b8a8e4a3ad01796570e51ee3763f76ae69ffc
= <=>
2022-01-07 09:55:24 -0400

    add provider state

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,18 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import CryptoDashboard from "./dashboard/crypto";
 import { getProviders } from "next-auth/react";
 
-
-const HomePage = ({ providers }) => {
+const HomePage = () => {
   const { data: session } = useSession();
-  return session ? <CryptoDashboard /> : <SignInPage providers ={providers}/>;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
+
+  return session ? <CryptoDashboard /> : <SignInPage providers={providers} />;
 };
 
-export async function getServerSideProps(context) {
-  const providers = await getProviders();
-  return {
-    props: { providers },
-  };
-}
 export default HomePage;
